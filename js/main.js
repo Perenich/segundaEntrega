89692b0af447a367c3d9981651468f5af73fcc54
Bruno Perenich <bgperenich@hotmail.com>
2022-07-28 23:30:21 -0300

    entrega28

--- js/main.js
+++ js/main.js
@@ -43,7 +43,7 @@ function primos(){
 
         formularioDos.reset()
 
-        return
+        return 
 }
 
 
@@ -71,3 +71,63 @@ function Calcular() {
 
 }
 
+const servicios = ['Esculpida', 'Esmaltada', 'Kipping']
+
+servicios.push('Art Nail')
+
+servicios.unshift('Esmaltado Sintetico')
+
+for (let i=0; i < servicios.length ; i++){
+    console.log('Servicio: '+ servicios[i])
+}
+
+
+console.log(servicios)
+
+
+let objeto1 = {
+    'propiedad': 'valor',
+    'propiedad2': 2,
+    'propiedad3': true,
+    'metodo': function(){
+        console.log('hola')
+    }
+}
+
+objeto1.metodo()
+
+function sumar(num1, num2){
+    return num1+num2
+}
+function restar(num1, num2){
+    return num1-num2
+}
+function dividir(num1, num2){
+    return num1/num2
+}
+function multiplicar(num1, num2){
+    return num1*num2
+}
+
+     
+function calculadora(operacion, num1, num2){
+    let resultado = operacion(num1, num2)
+    
+    return resultado
+}
+
+console.log(calculadora(sumar, 15,15))
+console.log(calculadora(restar, 20,10))
+console.log(calculadora(dividir, 100,5))
+console.log(calculadora(multiplicar, 3,15))
+
+const comidas = [
+    {'nombre': 'Hamburguesa', 'valor': 1000},
+    {'nombre': 'Tacos', 'valor': 1200},
+    {'nombre': 'Pizza de muzza', 'valor': 800},
+    {'nombre': 'tarta individuals', 'valor': 500}
+]
+
+const comidasVal = comidas.find( (e) => e.valor < 800)
+
+console.log(comidasVal)
